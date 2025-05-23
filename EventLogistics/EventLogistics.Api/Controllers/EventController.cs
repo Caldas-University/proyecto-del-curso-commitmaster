7294d272d6030084bbf97e6e7bf98550eac4545a
BGHcar <bryan.1701720431@ucaldas.edu.co>
2025-05-23 18:39:08 -0500

    merge

--- EventLogistics/EventLogistics.Api/Controllers/EventController.cs
+++ EventLogistics/EventLogistics.Api/Controllers/EventController.cs
@@ -4,3 +4,71 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+namespace EventLogistics.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetAll()
+        {
+            var events = await _eventRepository.GetAllAsync();
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetById(int id)
+        {
+            var eventItem = await _eventRepository.GetByIdAsync(id);
+            if (eventItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(eventItem);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Event>> Create(Event eventItem)
+        {
+            // Establecer valores predeterminados
+            eventItem.CreatedBy = eventItem.CreatedBy ?? "System";
+            eventItem.UpdatedBy = eventItem.UpdatedBy ?? "System";
+
+            var result = await _eventRepository.AddAsync(eventItem);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Event eventItem)
+        {
+            if (id != eventItem.Id)
+            {
+                return BadRequest();
+            }
+
+            eventItem.UpdatedBy = eventItem.UpdatedBy ?? "System";
+            var result = await _eventRepository.UpdateAsync(eventItem);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var eventItem = await _eventRepository.GetByIdAsync(id);
+            if (eventItem == null)
+            {
+                return NotFound();
+            }
+
+            await _eventRepository.DeleteAsync(eventItem);
+            return NoContent();
+        }
+
+    }
+}
