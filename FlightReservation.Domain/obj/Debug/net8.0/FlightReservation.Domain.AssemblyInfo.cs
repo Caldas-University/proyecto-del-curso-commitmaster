fbd4b2ba77182f54c0d45c0a6db24e2d34ae4049
leon55490 <leon55490@gmail.com>
2025-05-29 19:52:23 -0500

    ajustes

--- FlightReservation.Domain/obj/Debug/net8.0/FlightReservation.Domain.AssemblyInfo.cs
+++ FlightReservation.Domain/obj/Debug/net8.0/FlightReservation.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightReservation.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b7331435c42fda02d2e0bd32e6ec97bd76657d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4259249e2f43f8ff3197d182a7a2a413842f9099")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightReservation.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightReservation.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
